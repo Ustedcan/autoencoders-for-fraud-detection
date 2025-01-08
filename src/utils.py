161a0e3af324714a8530962f709fe885cf31969d
Ustedcan <clincango_425@hotmail.com>
2025-01-07 20:33:17 -0500

    Corrige la redundancia en la funcion concatenar

--- src/utils.py
+++ src/utils.py
@@ -1,23 +1,13 @@
 import pandas as pd
+
 def concatenate(*dfs):
     """
-    This fuction takes difrent dataframes and concatenates them into one dataframe.
-    axis=0 is used to concatenate the dataframes vertically.
-
-    *dfs insted of df1, df2, df3, ... dfn, *dfs is used to allow a variable number of arguments.
+    Concatenates multiple dataframes vertically.
 
     Args:
-    - *dfs: a variable number of pandas dataframes
+    - *dfs: Variable number of pandas dataframes.
 
     Returns:
-    - A pandas dataframe that contains all the dataframes in *dfs concatenated vertically.   
-    
-    Example:
-    -------------
-    df1 = pd.DataFrame({'A': [1, 2], 'B': [3, 4]})
-    df2 = pd.DataFrame({'A': [5, 6], 'B': [7, 8]})
-    
-    result = concatenate(df1, df2)
-    print(result)
+    - A pandas dataframe with all input dataframes concatenated vertically.
     """
     return pd.concat(dfs, axis=0, ignore_index=True)
